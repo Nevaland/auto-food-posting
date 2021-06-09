87c88ff5d07aea25525c1b5698dfe1f9c3e70254
Nevaland <deanh0716@gmail.com>
2021-06-10 01:13:11 +0900

    Add test_food_crawling

---
+++ tests.py
@@ -0,0 +1,19 @@
+import os
+from food_crawler import food_crawling, SCREENSHOT_FN
+
+
+def test_food_crawling():
+    if os.path.isfile(SCREENSHOT_FN):
+        os.remove(SCREENSHOT_FN)
+    food_crawling()
+    if not os.path.isfile(SCREENSHOT_FN):
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    if test_food_crawling():
+        print("[SUCCESS] food_crawling() geneated " + SCREENSHOT_FN + " File.")
+    else:
+        print("[ERROR] food_crawling() failed to geneate " +
+              SCREENSHOT_FN + " File.")
